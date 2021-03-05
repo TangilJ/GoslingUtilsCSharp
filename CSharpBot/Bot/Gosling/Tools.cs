a5c2d859fd5a4cdaf07b62e724f035164e016fd8
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-05 18:37:46 +0000

    Create empty Tools class for strategic tools

---
+++ CSharpBot/Bot/Gosling/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Bot.Gosling
+{
+    /// <summary>
+    /// This class is for strategic tools.
+    /// </summary>
+    public static class Tools
+    {
+        public static void FindHits(GoslingAgent agent, Dictionary<string, (Vector3, Vector3)> targets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
