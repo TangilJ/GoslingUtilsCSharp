78a6c0c48d81d903dd316fc9ad2518a4a48b27e2
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-05 18:32:28 +0000

    Create empty Atba and Kickoff routines for example

---
+++ CSharpBot/Bot/Gosling/Routines/Atba.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.Gosling.Routines
+{
+    public class Atba : IRoutine
+    {
+        public void Run(GoslingAgent agent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CSharpBot/Bot/Gosling/Routines/Kickoff.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.Gosling.Routines
+{
+    public class Kickoff : IRoutine
+    {
+        public void Run(GoslingAgent agent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
