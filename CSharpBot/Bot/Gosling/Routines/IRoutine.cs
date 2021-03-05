a69a9b6bd79180a9ea9236bdc1f7750664c91dc7
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-05 15:39:40 +0000

    Create IRoutine interface for mechanical tasks

---
+++ CSharpBot/Bot/Gosling/Routines/IRoutine.cs
@@ -0,0 +1,14 @@
+﻿namespace Bot.Gosling.Routines
+{
+    /// <summary>
+    /// Interface for mechanical tasks, called "routines", that the bot can do.
+    /// </summary>
+    /// <remarks>
+    /// Porting note: the original code for GoslingUtils does not use an interface as Python doesn't have them. We use
+    /// an interface here for routines as it's the most idiomatic option for C#.
+    /// </remarks>
+    public interface IRoutine
+    {
+        void Run(GoslingAgent agent);
+    }
+}
