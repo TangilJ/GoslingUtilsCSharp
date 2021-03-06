dc72797437ed38b37c765cd8a1b8ed8c5a44f493
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-06 16:55:14 +0000

    Implement constructors for AerialShot & JumpShot

--- CSharpBot/Bot/Gosling/Routines/AerialShot.cs
+++ CSharpBot/Bot/Gosling/Routines/AerialShot.cs
@@ -3,12 +3,48 @@
 namespace Bot.Gosling.Routines
 {
     /// <summary>
-    /// Very similar to JumpShot(), but instead designed to hit targets above 300uu.
+    /// Very similar to JumpShot(), but instead designed to hit targets above 300uu.<br/>
     /// ***This routine is a WIP*** It does not currently hit the ball very hard, nor does it like to be accurate above
     /// 600uu or so.
     /// </summary>
     public class AerialShot : IShotRoutine
     {
+        /// <summary>
+        /// The direction we intend to hit the ball in
+        /// </summary>
+        private Vector3 shotVector;
+
+        /// <summary>
+        /// The point we hit the ball at
+        /// </summary>
+        private Vector3 intercept;
+
+        /// <summary>
+        /// Dictates when (how late) we jump, much later than in JumpShot because we can take advantage of a double jump
+        /// </summary>
+        private float jumpThreshold = 600;
+
+        /// <summary>
+        /// What time we began our jump at
+        /// </summary>
+        private float jumpTime = 0;
+
+        /// <summary>
+        /// If we need a second jump we have to let go of the jump button for 3 frames, this counts how many frames we have let go for
+        /// </summary>
+        private int counter = 0;
+        
+        // Porting note: the ratio parameter is unused but the original code has it so we keep it in the port
+        public AerialShot(Vector3 ballLocation, float interceptTime, Vector3 shotVector, float ratio)
+        {
+            BallLocation = ballLocation;
+            InterceptTime = interceptTime;
+
+            this.shotVector = shotVector;
+
+            intercept = ballLocation - (shotVector * 120);
+        }
+
         public void Run(GoslingAgent agent)
         {
             throw new System.NotImplementedException();
--- CSharpBot/Bot/Gosling/Routines/JumpShot.cs
+++ CSharpBot/Bot/Gosling/Routines/JumpShot.cs
@@ -3,11 +3,66 @@
 namespace Bot.Gosling.Routines
 {
     /// <summary>
-    /// Hits a target point at a target time towards a target direction.
+    /// Hits a target point at a target time towards a target direction.<br/>
     /// Target must be no higher than 300uu unless you're feeling lucky. 
     /// </summary>
     public class JumpShot : IShotRoutine
     {
+        /// <summary>
+        /// The direction we intend to hit the ball in
+        /// </summary>
+        private Vector3 shotVector;
+
+        /// <summary>
+        /// The point we hit the ball at
+        /// </summary>
+        private Vector3 intercept;
+
+        /// <summary>
+        /// The point we dodge at.<br/>
+        /// 173 is the 93uu ball radius + a bit more to account for the car's hitbox
+        /// </summary>
+        private Vector3 dodgePoint;
+
+        /// <summary>
+        /// Ratio is how aligned the car is. Low ratios (&lt;0.5) aren't likely to be hit properly.
+        /// </summary>
+        private float ratio;
+
+        /// <summary>
+        /// Whether the car should attempt this backwards
+        /// </summary>
+        private float direction;
+
+        /// <summary>
+        /// Intercept speed not implemented
+        /// </summary>
+        private float speedDesired;
+
+        /// <summary>
+        /// Controls how soon car will jump based on acceleration required. max 584<br/>
+        /// Bigger = later, which allows more time to align with shot vector<br/>
+        /// Smaller = sooner
+        /// </summary>
+        private float jumpThreshold = 400;
+
+        private bool jumping = false;
+        private bool dodging = false;
+        private int counter = 0;
+
+        public JumpShot(
+            Vector3 ballLocation, float interceptTime, Vector3 shotVector, float ratio,
+            float direction = 1, float speed = 2300
+        )
+        {
+            BallLocation = ballLocation;
+            InterceptTime = interceptTime;
+            this.shotVector = shotVector;
+            this.ratio = ratio;
+            this.direction = direction;
+            speedDesired = speed;
+        }
+
         public void Run(GoslingAgent agent)
         {
             throw new System.NotImplementedException();
