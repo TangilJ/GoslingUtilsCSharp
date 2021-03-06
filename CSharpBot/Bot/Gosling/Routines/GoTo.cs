201c11471e44b88c4d2b5b51230668737e5bee80
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-06 22:19:38 +0000

    Implement GoTo and ShortShot routines

--- CSharpBot/Bot/Gosling/Routines/GoTo.cs
+++ CSharpBot/Bot/Gosling/Routines/GoTo.cs
@@ -1,14 +1,70 @@
-﻿namespace Bot.Gosling.Routines
+﻿using System;
+using System.Numerics;
+using System.Windows.Media;
+
+namespace Bot.Gosling.Routines
 {
     /// <summary>
-    /// Drives towards a designated (stationary) target.
-    /// Optional vector controls where the car should be pointing upon reaching the target.
+    /// Drives towards a designated (stationary) target.<br/>
+    /// Optional vector controls where the car should be pointing upon reaching the target.<br/>
+    /// TODO - slow down if target is inside our turn radius.
     /// </summary>
     public class GoTo : IRoutine
     {
+        private Vector3 target;
+        private Vector3? vector;
+        private float direction;
+
+        public GoTo(Vector3 target, Vector3? vector = null, float direction = 1)
+        {
+            this.target = target;
+            this.vector = vector;
+            this.direction = direction;
+        }
+
         public void Run(GoslingAgent agent)
         {
-            throw new System.NotImplementedException();
+            var carToTarget = target - agent.Me.Location;
+            var distanceRemaining = carToTarget.Flatten().Length();
+
+            agent.Line(target - new Vector3(0, 0, 500), target + new Vector3(0, 0, 500), Color.FromRgb(255, 0, 255));
+
+            Vector3 finalTarget;
+            if (vector.HasValue)
+            {
+                // See comments for adjustment in JumpShot or Aerial for explanation
+                var sideOfVector =
+                    Utils.Sign(Vector3.Dot(Vector3.Cross(vector.Value, Vector3.UnitZ), carToTarget));
+                var carToTargetPerp = Vector3.Normalize(Vector3.Cross(carToTarget, new Vector3(0, 0, sideOfVector)));
+                var adjustment = carToTarget.Angle(vector.Value) * distanceRemaining / 3.14f;
+                finalTarget = target + (carToTargetPerp * adjustment);
+            }
+            else
+                finalTarget = target;
+
+            // Some adjustment to the final target to ensure it's inside the field and we don't try to drive through any
+            // goalposts to reach it
+            if (Math.Abs(agent.Me.Location.Y) > 5150)
+                finalTarget.X = Utils.Cap(finalTarget.X, -750, 750);
+
+            var localTarget = agent.Me.Local(finalTarget - agent.Me.Location);
+
+            var (_, angleY, _) = Utils.DefaultPd(agent, localTarget, direction);
+            Utils.DefaultThrottle(agent, 2300, direction);
+
+            agent.Controller.Boost = false;
+            agent.Controller.Handbrake = Math.Abs(angleY) > 2.3 || agent.Controller.Handbrake;
+
+            var velocity = 1 + agent.Me.Velocity.Length();
+            if (distanceRemaining < 350)
+                agent.Stack.Pop();
+            else if (Math.Abs(angleY) < 0.05 && velocity > 600 && velocity < 2150 &&
+                     distanceRemaining / velocity > 2)
+                agent.Stack.Push(new Flip(localTarget));
+            else if (Math.Abs(angleY) > 2.8 && velocity < 200)
+                agent.Stack.Push(new Flip(localTarget, cancel: true));
+            else if (agent.Me.Airborne)
+                agent.Stack.Push(new Recovery(target));
         }
     }
 }
--- CSharpBot/Bot/Gosling/Routines/ShortShot.cs
+++ CSharpBot/Bot/Gosling/Routines/ShortShot.cs
@@ -1,15 +1,62 @@
-﻿namespace Bot.Gosling.Routines
-{
+﻿using System;
+using System.Numerics;
+using System.Windows.Media;
 
+namespace Bot.Gosling.Routines
+{
     /// <summary>
-    /// This routine drives towards the ball and attempts to hit it towards a given target.
+    /// This routine drives towards the ball and attempts to hit it towards a given target.<br/>
     /// It does not require ball prediction and kinda guesses at where the ball will be on its own.
     /// </summary>
     public class ShortShot : IRoutine
     {
+        private Vector3 target;
+
+        public ShortShot(Vector3 target)
+        {
+            this.target = target;
+        }
+
         public void Run(GoslingAgent agent)
         {
-            throw new System.NotImplementedException();
+            var carToBall = agent.Ball.Physics.Location - agent.Me.Location;
+            var distance = carToBall.Length();
+            carToBall = Vector3.Normalize(carToBall);
+            var ballToTarget = Vector3.Normalize(target - agent.Ball.Physics.Location);
+
+            var relativeVelocity = Vector3.Dot(carToBall, agent.Me.Velocity - agent.Ball.Physics.Velocity);
+            float eta;
+            if (relativeVelocity != 0)
+                eta = Utils.Cap(distance / Utils.Cap(relativeVelocity, 400, 2300), 0, 1.5f);
+            else
+                eta = 1.5f;
+
+            // If we are approaching the ball from the wrong side the car will try to only hit the very edge of the ball
+            var leftVector = Vector3.Cross(carToBall, Vector3.UnitZ);
+            var rightVector = Vector3.Cross(carToBall, -Vector3.UnitZ);
+            var targetVector = Vector3.Clamp(-ballToTarget, leftVector, rightVector);
+            var finalTarget = agent.Ball.Physics.Location + (targetVector * (distance / 2));
+
+            // Some adjustment to the final target to ensure we don't try to dirve through any goalposts to reach it
+            if (Math.Abs(agent.Me.Location.Y) > 5150)
+                finalTarget.X = Utils.Cap(finalTarget.X, -750, 750);
+
+            agent.Line(finalTarget - new Vector3(0, 0, 100), finalTarget + new Vector3(0, 0, 100), Colors.White);
+
+            var (_, angleY, _) = Utils.DefaultPd(agent, agent.Me.Local(finalTarget - agent.Me.Location));
+            Utils.DefaultThrottle(agent,
+                distance > 1600
+                    ? 2300
+                    : 2300 - Utils.Cap(1600 * Math.Abs(angleY), 0, 2050)
+            );
+            agent.Controller.Boost = agent.Me.Airborne || Math.Abs(angleY) > 0.3 ? false : agent.Controller.Boost;
+            agent.Controller.Handbrake = Math.Abs(angleY) > 2.3 || agent.Controller.Handbrake;
+            
+            if (Math.Abs(angleY) < 0.05 && (eta < 0.45 || distance < 150))
+            {
+                agent.Stack.Pop();
+                agent.Stack.Push(new Flip(agent.Me.Local(carToBall)));
+            }
         }
     }
 }
