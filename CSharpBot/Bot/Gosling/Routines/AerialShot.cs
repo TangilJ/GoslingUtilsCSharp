a6d6d63c6c7ee88f2a044dd08fe1b94b41a0be76
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-07 00:27:58 +0000

    Implement GoToBoost and AerialShot routines

--- CSharpBot/Bot/Gosling/Routines/AerialShot.cs
+++ CSharpBot/Bot/Gosling/Routines/AerialShot.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
+using System.Windows.Media;
 
 namespace Bot.Gosling.Routines
 {
@@ -33,7 +35,7 @@ namespace Bot.Gosling.Routines
         /// If we need a second jump we have to let go of the jump button for 3 frames, this counts how many frames we have let go for
         /// </summary>
         private int counter = 0;
-        
+
         // Porting note: the ratio parameter is unused but the original code has it so we keep it in the port
         public AerialShot(Vector3 ballLocation, float interceptTime, Vector3 shotVector, float ratio)
         {
@@ -47,7 +49,118 @@ namespace Bot.Gosling.Routines
 
         public void Run(GoslingAgent agent)
         {
-            throw new System.NotImplementedException();
+            var rawTimeRemaining = InterceptTime - agent.Time;
+            // Capping rawTimeRemaining above 0 to prevent division problems
+            var timeRemaining = Utils.Cap(rawTimeRemaining, 0.01f, 10);
+
+            var carToBall = BallLocation - agent.Me.Location;
+            // Whether we are to the left or right of the shot vector
+            var sideOfShot = Utils.Sign(Vector3.Dot(Vector3.Cross(shotVector, Vector3.UnitZ), carToBall));
+
+            var carToIntercept = intercept - agent.Me.Location;
+            var carToInterceptPerp = Vector3.Cross(carToIntercept, new Vector3(0, 0, sideOfShot)); // Perpendicular
+            var flatDistanceRemaining = carToIntercept.Flatten().Length();
+
+            var speedRequired = flatDistanceRemaining / timeRemaining;
+            // When still on the ground we pretend gravity doesn't exist, for better or worse
+            var accelerationRequired = Utils.Backsolve(intercept, agent.Me, timeRemaining, 325);
+            var localAccelerationRequired = agent.Me.Local(accelerationRequired);
+
+            // The adjustment causes the car to circle around the dodge point in an effort to line up with the shot vector
+            // The adjustment slowly decreases to 0 as the bot nears the time to jump
+            var adjustment = carToIntercept.Angle(shotVector) * flatDistanceRemaining / 1.57f; // Size of adjustment
+            // Factoring in how close to jump we are
+            adjustment *= Utils.Cap(jumpThreshold - accelerationRequired.Z, 0, jumpThreshold) / jumpThreshold;
+            // We don't adjust the final target if we are already jumping
+            var finalTarget = intercept + (
+                jumpTime == 0
+                    ? Vector3.Normalize(carToInterceptPerp) * adjustment
+                    : Vector3.Zero
+            );
+
+            // Some adjustment to the final target to ensure it's inside the field and we don't try to drive through any
+            // goalposts to reach it
+            if (Math.Abs(agent.Me.Location.Y) > 5150)
+                finalTarget.X = Utils.Cap(finalTarget.X, -750, 750);
+
+            var localTarget = agent.Me.Local(finalTarget - agent.Me.Location);
+
+            // Drawing debug lines to show the dodge point and final target (which differs due to the adjustment)
+            agent.Line(agent.Me.Location, intercept);
+            agent.Line(intercept - new Vector3(0, 0, 100), intercept + new Vector3(0, 0, 100), Colors.Red);
+            agent.Line(finalTarget - new Vector3(0, 0, 100), finalTarget + new Vector3(0, 0, 100),
+                Color.FromRgb(0, 255, 0));
+
+            var (angleX, angleY, _) = Utils.DefaultPd(agent, localTarget);
+
+            float timeSinceJump;
+            if (jumpTime == 0)
+            {
+                Utils.DefaultThrottle(agent, speedRequired);
+                agent.Controller.Boost = Math.Abs(angleY) > 0.3 || agent.Me.Airborne ? false : agent.Controller.Boost;
+                agent.Controller.Handbrake = Math.Abs(angleY) > 2.3 || agent.Controller.Handbrake;
+
+                var velocityRequired = carToIntercept / timeRemaining;
+                var velAbs = velocityRequired.Abs();
+                var goodSlope = velocityRequired.Z / Utils.Cap(velAbs.X + velAbs.Y, 1, 10000) > 0.15;
+                var dot = Vector3.Dot(
+                    Vector3.Normalize(agent.Me.Velocity.Flatten()),
+                    Vector3.Normalize(accelerationRequired.Flatten())
+                );
+                if (goodSlope && localAccelerationRequired.Z > jumpThreshold && dot > 0.8)
+                    // Switch into the jump when the upward acceleration required reaches our threshold, hopefully we
+                    // have aligned already...
+                    jumpTime = agent.Time;
+            }
+            else
+            {
+                timeSinceJump = agent.Time - jumpTime;
+
+                // While airborne we boost if we're within 30 degrees of our local acceleration requirement
+                if (agent.Me.Airborne && localAccelerationRequired.Length() * timeRemaining > 90)
+                {
+                    (angleX, angleY, _) = Utils.DefaultPd(agent, localAccelerationRequired);
+                    if (Math.Abs(angleX) + Math.Abs(angleY) < 0.45)
+                        agent.Controller.Boost = true;
+                }
+                else
+                {
+                    finalTarget -= new Vector3(0, 0, 45);
+                    var localFinalTarget = agent.Me.Local(finalTarget - agent.Me.Location);
+                    (angleX, angleY, _) = Utils.DefaultPd(agent, localFinalTarget);
+                }
+
+                if (counter == 0 && (timeSinceJump <= 0.2 && localAccelerationRequired.Z > 0))
+                    // Hold the jump button up to 0.2 seconds to get the most acceleration from the first jump
+                    agent.Controller.Jump = true;
+                else if (timeSinceJump > 0.2 && counter < 3)
+                {
+                    // Release the jump button for 3 ticks
+                    agent.Controller.Jump = false;
+                    agent.Controller.Pitch = 0;
+                    agent.Controller.Yaw = 0;
+                    agent.Controller.Roll = 0;
+                    counter++;
+                }
+                else if (localAccelerationRequired.Z > 300 && counter == 3)
+                {
+                    // The acceleration from the second jump is instant, so we only do it for 1 frame
+                    agent.Controller.Jump = true;
+                    agent.Controller.Pitch = 0;
+                    agent.Controller.Yaw = 0;
+                    agent.Controller.Roll = 0;
+                    counter++;
+                }
+            }
+
+            if (rawTimeRemaining < -0.25)
+            {
+                agent.Pop();
+                agent.Push(new Recovery());
+            }
+
+            if (!Utils.ShotValid(agent, this, 90))
+                agent.Pop();
         }
 
         public float InterceptTime { get; set; }
--- CSharpBot/Bot/Gosling/Routines/GoToBoost.cs
+++ CSharpBot/Bot/Gosling/Routines/GoToBoost.cs
@@ -1,14 +1,77 @@
-﻿namespace Bot.Gosling.Routines
+﻿using System;
+using System.Numerics;
+using System.Windows.Media;
+using Bot.Gosling.Objects;
+
+namespace Bot.Gosling.Routines
 {
     /// <summary>
-    /// Very similar to goto() but designed for grabbing boost.
+    /// Very similar to goto() but designed for grabbing boost.<br/>
     /// If a target is provided the bot will try to be facing the target as it passes over the boost.
     /// </summary>
     public class GoToBoost : IRoutine
     {
+        private BoostObject boost;
+        private Vector3? target;
+
+        public GoToBoost(BoostObject boost, Vector3? target = null)
+        {
+            this.boost = boost;
+            this.target = target;
+        }
+
         public void Run(GoslingAgent agent)
         {
-            throw new System.NotImplementedException();
+            var carToBoost = boost.Location - agent.Me.Location;
+
+            var distanceRemaining = carToBoost.Flatten().Length();
+
+            agent.Line(
+                boost.Location - new Vector3(0, 0, 500),
+                boost.Location + new Vector3(0, 0, 500),
+                Color.FromRgb(0, 255, 0)
+            );
+
+            float adjustment;
+            Vector3 finalTarget;
+            float carToTarget;
+            if (target.HasValue)
+            {
+                var vector = Vector3.Normalize(target.Value - boost.Location);
+                var sideOfVector = Utils.Sign(Vector3.Dot(Vector3.Cross(vector, Vector3.UnitZ), carToBoost));
+                var carToBoostPerp = Vector3.Normalize(Vector3.Cross(carToBoost, new Vector3(0, 0, sideOfVector)));
+                adjustment = carToBoost.Angle(vector) * distanceRemaining / 3.14f;
+                finalTarget = boost.Location + (carToBoostPerp * adjustment);
+                carToTarget = (target.Value - agent.Me.Location).Length();
+            }
+            else
+            {
+                adjustment = 9999;
+                carToTarget = 0;
+                finalTarget = boost.Location;
+            }
+
+            // Some adjustment to the final target to ensure it's inside the field and we don't try to drive through any
+            // goalposts to reach it
+            if (Math.Abs(agent.Me.Location.Y) > 5150)
+                finalTarget.X = Utils.Cap(finalTarget.X, -750, 750);
+
+            var localTarget = agent.Me.Local(finalTarget - agent.Me.Location);
+
+            var (_, angleY, _) = Utils.DefaultPd(agent, localTarget);
+            Utils.DefaultThrottle(agent, 2300);
+
+            agent.Controller.Boost = Math.Abs(angleY) < 0.3 && boost.Large;
+            agent.Controller.Handbrake = Math.Abs(angleY) > 2.3 || agent.Controller.Handbrake;
+
+            var velocity = 1 + agent.Me.Velocity.Length();
+            if (!boost.Active || agent.Me.Boost >= 99 || distanceRemaining < 350)
+                agent.Pop();
+            else if (agent.Me.Airborne)
+                agent.Push(new Recovery(target));
+            else if (Math.Abs(angleY) < 0.05 && velocity > 600 && velocity < 2150 && distanceRemaining / velocity > 2 
+                || adjustment < 90 && carToTarget / velocity > 2.0)
+                agent.Push(new Flip(localTarget));
         }
     }
 }
